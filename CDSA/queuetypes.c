9100fc11a21e91dce80e0637c3895a315c2cd47d
Roshan Palaniappan <138195422+RODEEKS@users.noreply.github.com>
2024-05-23 15:52:29 +0530

    Add files via upload

---
+++ CDSA/queuetypes.c
@@ -0,0 +1,369 @@
+// Circular
+
+// The main advantage of a circular queue over a simple queue is better memory utilization. 
+// If the last position is full and the first position is empty, we can insert an element in the first position. 
+// This action is not possible in a simple queue.
+
+// Circular Queue implementation in C
+
+#include <stdio.h>
+
+#define SIZE 5
+
+int items[SIZE];
+int front = -1, rear = -1;
+
+// Check if the queue is full
+int isFull() {
+  if ((front == rear + 1) || (front == 0 && rear == SIZE - 1)) return 1;
+  return 0;
+}
+
+// Check if the queue is empty
+int isEmpty() {
+  if (front == -1) return 1;
+  return 0;
+}
+
+// Adding an element
+void enQueue(int element) {
+  if (isFull())
+    printf("\n Queue is full!! \n");
+  else {
+    if (front == -1) front = 0;
+    rear = (rear + 1) % SIZE;
+    items[rear] = element;
+    printf("\n Inserted -> %d", element);
+  }
+}
+
+// Removing an element
+int deQueue() {
+  int element;
+  if (isEmpty()) {
+    printf("\n Queue is empty !! \n");
+    return (-1);
+  } else {
+    element = items[front];
+    if (front == rear) {
+      front = -1;
+      rear = -1;
+    } 
+    // Q has only one element, so we reset the 
+    // queue after dequeing it. ?
+    else {
+      front = (front + 1) % SIZE;
+    }
+    printf("\n Deleted element -> %d \n", element);
+    return (element);
+  }
+}
+
+// Display the queue
+void display() {
+  int i;
+  if (isEmpty())
+    printf(" \n Empty Queue\n");
+  else {
+    printf("\n Front -> %d ", front);
+    printf("\n Items -> ");
+    for (i = front; i != rear; i = (i + 1) % SIZE) {
+      printf("%d ", items[i]);
+    }
+    printf("%d ", items[i]);
+    printf("\n Rear -> %d \n", rear);
+  }
+}
+
+int main() {
+  // Fails because front = -1
+  deQueue();
+
+  enQueue(1);
+  enQueue(2);
+  enQueue(3);
+  enQueue(4);
+  enQueue(5);
+
+  // Fails to enqueue because front == 0 && rear == SIZE - 1
+  enQueue(6);
+
+  display();
+  deQueue();
+
+  display();
+
+  enQueue(7);
+  display();
+
+  // Fails to enqueue because front == rear + 1
+  enQueue(8);
+
+  return 0;
+}
+
+// Priority
+
+// A priority queue is a special type of queue in which each element is associated with a priority and is served according to its priority. 
+// If elements with the same priority occur, they are served according to their order in the queue.
+
+// Priority Queue implementation in C
+
+#include <stdio.h>
+int size = 0;
+void swap(int *a, int *b) {
+  int temp = *b;
+  *b = *a;
+  *a = temp;
+}
+
+// Function to heapify the tree
+void heapify(int array[], int size, int i) {
+  if (size == 1) {
+    printf("Single element in the heap");
+  } else {
+    // Find the largest among root, left child and right child
+    int largest = i;
+    int l = 2 * i + 1;
+    int r = 2 * i + 2;
+    if (l < size && array[l] > array[largest])
+      largest = l;
+    if (r < size && array[r] > array[largest])
+      largest = r;
+
+    // Swap and continue heapifying if root is not largest
+    if (largest != i) {
+      swap(&array[i], &array[largest]);
+      heapify(array, size, largest);
+    }
+  }
+}
+
+// Function to insert an element into the tree
+void insert(int array[], int newNum) {
+  if (size == 0) {
+    array[0] = newNum;
+    size += 1;
+  } else {
+    array[size] = newNum;
+    size += 1;
+    for (int i = size / 2 - 1; i >= 0; i--) {
+      heapify(array, size, i);
+    }
+  }
+}
+
+// Function to delete an element from the tree
+void deleteRoot(int array[], int num) {
+  int i;
+  for (i = 0; i < size; i++) {
+    if (num == array[i])
+      break;
+  }
+
+  swap(&array[i], &array[size - 1]);
+  size -= 1;
+  for (int i = size / 2 - 1; i >= 0; i--) {
+    heapify(array, size, i);
+  }
+}
+
+// Print the array
+void printArray(int array[], int size) {
+  for (int i = 0; i < size; ++i)
+    printf("%d ", array[i]);
+  printf("\n");
+}
+
+// Driver code
+int main() {
+  int array[10];
+
+  insert(array, 3);
+  insert(array, 4);
+  insert(array, 9);
+  insert(array, 5);
+  insert(array, 2);
+
+  printf("Max-Heap array: ");
+  printArray(array, size);
+
+  deleteRoot(array, 4);
+
+  printf("After deleting an element: ");
+
+  printArray(array, size);
+}
+
+
+// Double Ended
+
+// In a double ended queue, insertion and removal of elements can be performed from either from the front or rear.
+// Thus, it does not follow the FIFO (First In First Out) rule.
+
+// Deque implementation in C
+
+#include <stdio.h>
+
+#define MAX 10
+
+void addFront(int *, int, int *, int *);
+void addRear(int *, int, int *, int *);
+int delFront(int *, int *, int *);
+int delRear(int *, int *, int *);
+void display(int *);
+int count(int *);
+
+int main() {
+  int arr[MAX];
+  int front, rear, i, n;
+
+  front = rear = -1;
+  for (i = 0; i < MAX; i++)
+    arr[i] = 0;
+
+  addRear(arr, 5, &front, &rear);
+  addFront(arr, 12, &front, &rear);
+  addRear(arr, 11, &front, &rear);
+  addFront(arr, 5, &front, &rear);
+  addRear(arr, 6, &front, &rear);
+  addFront(arr, 8, &front, &rear);
+
+  printf("\nElements in a deque: ");
+  display(arr);
+
+  i = delFront(arr, &front, &rear);
+  printf("\nremoved item: %d", i);
+
+  printf("\nElements in a deque after deletion: ");
+  display(arr);
+
+  addRear(arr, 16, &front, &rear);
+  addRear(arr, 7, &front, &rear);
+
+  printf("\nElements in a deque after addition: ");
+  display(arr);
+
+  i = delRear(arr, &front, &rear);
+  printf("\nremoved item: %d", i);
+
+  printf("\nElements in a deque after deletion: ");
+  display(arr);
+
+  n = count(arr);
+  printf("\nTotal number of elements in deque: %d", n);
+}
+
+void addFront(int *arr, int item, int *pfront, int *prear) {
+  int i, k, c;
+
+  if (*pfront == 0 && *prear == MAX - 1) {
+    printf("\nDeque is full.\n");
+    return;
+  }
+
+  if (*pfront == -1) {
+    *pfront = *prear = 0;
+    arr[*pfront] = item;
+    return;
+  }
+
+  if (*prear != MAX - 1) {
+    c = count(arr);
+    k = *prear + 1;
+    for (i = 1; i <= c; i++) {
+      arr[k] = arr[k - 1];
+      k--;
+    }
+    arr[k] = item;
+    *pfront = k;
+    (*prear)++;
+  } else {
+    (*pfront)--;
+    arr[*pfront] = item;
+  }
+}
+
+void addRear(int *arr, int item, int *pfront, int *prear) {
+  int i, k;
+
+  if (*pfront == 0 && *prear == MAX - 1) {
+    printf("\nDeque is full.\n");
+    return;
+  }
+
+  if (*pfront == -1) {
+    *prear = *pfront = 0;
+    arr[*prear] = item;
+    return;
+  }
+
+  if (*prear == MAX - 1) {
+    k = *pfront - 1;
+    for (i = *pfront - 1; i < *prear; i++) {
+      k = i;
+      if (k == MAX - 1)
+        arr[k] = 0;
+      else
+        arr[k] = arr[i + 1];
+    }
+    (*prear)--;
+    (*pfront)--;
+  }
+  (*prear)++;
+  arr[*prear] = item;
+}
+
+int delFront(int *arr, int *pfront, int *prear) {
+  int item;
+
+  if (*pfront == -1) {
+    printf("\nDeque is empty.\n");
+    return 0;
+  }
+
+  item = arr[*pfront];
+  arr[*pfront] = 0;
+
+  if (*pfront == *prear)
+    *pfront = *prear = -1;
+  else
+    (*pfront)++;
+
+  return item;
+}
+
+int delRear(int *arr, int *pfront, int *prear) {
+  int item;
+
+  if (*pfront == -1) {
+    printf("\nDeque is empty.\n");
+    return 0;
+  }
+
+  item = arr[*prear];
+  arr[*prear] = 0;
+  (*prear)--;
+  if (*prear == -1)
+    *pfront = -1;
+  return item;
+}
+
+void display(int *arr) {
+  int i;
+
+  printf("\n front:  ");
+  for (i = 0; i < MAX; i++)
+    printf("  %d", arr[i]);
+  printf("  :rear");
+}
+
+int count(int *arr) {
+  int c = 0, i;
+
+  for (i = 0; i < MAX; i++) {
+    if (arr[i] != 0)
+      c++;
+  }
+  return c;
+}
