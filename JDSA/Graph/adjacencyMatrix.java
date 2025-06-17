fd26e1ef5372af4bbdcd76e1ea9e2ccea9bcc450
Roshan Palaniappan <138195422+ROSHAN0931@users.noreply.github.com>
2025-06-17 21:19:40 +0530

    adjacencyMatrix.java

--- JDSA/Graph/adjacencyMatrix.java
+++ JDSA/Graph/adjacencyMatrix.java
@@ -1,49 +1,41 @@
-class adjecencyMatrix {
-    private int[][] adjacencyMatrix;
+class Graph {
+    private int[][] adjMatrix;
     private int numVertices;
 
-    // Constructor
-    public adjecencyMatrix(int numVertices) {
-        this.numVertices = numVertices;
-        adjacencyMatrix = new int[numVertices][numVertices];
+    public Graph(int vertices) {
+        this.numVertices = vertices;
+        adjMatrix = new int[vertices][vertices];
     }
 
-    // Add edge
-    public void addEdge(int source, int destination) {
-        // Adding an edge to the graph (for undirected graph, add both ways)
-        adjacencyMatrix[source][destination] = 1;
-        adjacencyMatrix[destination][source] = 1;
+    public void addEdge(int i, int j) {
+        adjMatrix[i][j] = 1;
+        adjMatrix[j][i] = 1; 
     }
 
-    // Remove edge
-    public void removeEdge(int source, int destination) {
-        // Removing an edge from the graph
-        adjacencyMatrix[source][destination] = 0;
-        adjacencyMatrix[destination][source] = 0;
-    }
-
-    // Print the adjacency matrix
     public void printGraph() {
         System.out.println("Adjacency Matrix:");
         for (int i = 0; i < numVertices; i++) {
             for (int j = 0; j < numVertices; j++) {
-                System.out.print(adjacencyMatrix[i][j] + " ");
+                System.out.print(adjMatrix[i][j] + " ");
             }
             System.out.println();
         }
     }
+}
 
+
+public class Main {
     public static void main(String[] args) {
-        // Create a graph with 4 vertices
-        adjecencyMatrix graph = new adjecencyMatrix(4);
+        Graph g = new Graph(5);  
 
-        // Add edges
-        graph.addEdge(0, 1);
-        graph.addEdge(0, 2);
-        graph.addEdge(1, 2);
-        graph.addEdge(2, 3);
+        g.addEdge(0, 1);
+        g.addEdge(0, 4);
+        g.addEdge(1, 2);
+        g.addEdge(1, 3);
+        g.addEdge(1, 4);
+        g.addEdge(2, 3);
+        g.addEdge(3, 4);
 
-        // Print the adjacency matrix
-        graph.printGraph();
+        g.printGraph();
     }
 }
